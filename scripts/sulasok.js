c1f374adaecccec4b92948247cc6cf3daf5a40ba
Justin Gonzaga <justingonzaga333@gmail.com>
2025-09-27 18:01:25 +0800

    started js

--- scripts/sulasok.js
+++ scripts/sulasok.js
@@ -1,8 +1,48 @@
 //Represents a list
+// Whole Data for the products
 const products = [{
+
   image: 'images/M69.jpg',
   name: 'M69: Rifle',
   rating: {
-    
-  }
+    stars: 3,
+    rating: 90  
+  },
+  priceCents: 690000 
+}, {
+
+  image: 'images/vape.png',
+  name: 'Vape',
+  rating: {
+    star: 3,
+    rating: 69
+  },
+  priceCents: 2000
+}, {
+
+  image: 'images/cherry-mobile.jpg',
+  name: 'Cherry Mobile',
+  rating: {
+    star: 3,  
+    rating: 78
+  },
+  priceCents: 560000
+}, {
+
+  image: 'images/c69.jpg',
+  name: 'C69',
+  rating: {
+    star: 3,
+    rating: 99
+  },
+  priceCents: 96000000
+}, {
+
+  images: 'images/white-tee.jpg',
+  name: 'White T-Shirt',
+  rating: {
+    star: 3,
+    rating: 100
+  },
+  priceCents: 20000
 }];
