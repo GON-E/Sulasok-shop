11093a05c131ebc6c46c53457aa3d88bad3a00da
Justin Gonzaga <justingonzaga333@gmail.com>
2025-09-30 15:52:50 +0800

    Commit

---
+++ scripts/products.js
@@ -0,0 +1,55 @@
+const products = [{
+
+  image: 'images/M69.jpg',
+  name: 'M69: Rifle',
+  rating: {
+    star: 3,
+    count: 90
+  },
+  priceCents: 690000 
+}, {
+
+  image: 'images/vape.png',
+  name: 'Vape',
+  rating: {
+    star: 3,
+    count: 69
+  },
+  priceCents: 2000
+}, {
+
+  image: 'images/cherry-mobile.jpg',
+  name: 'Cherry Mobile',
+  rating: {
+    star: 3,  
+    count: 78
+  },
+  priceCents: 560000
+}, {
+
+  image: 'images/c69.jpg',
+  name: 'C69',
+  rating: {
+    star: 3,
+    count: 99
+  },
+  priceCents: 96000000
+}, {
+
+  image: 'images/whiteT.jpg',
+  name: 'White T-Shirt',
+  rating: {
+    star: 3,
+    count: 100
+  },
+  priceCents: 20000
+}, {
+  image: 'images/dildo.jpg',
+  name: "Dildo ni neel",
+  rating: {
+    star: 3,
+    count: 69
+  },
+  priceCents: 6969
+}
+];
